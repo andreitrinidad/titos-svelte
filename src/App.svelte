db206405313890730b31b596f6375026b729ff15
andreitrinidad <andreitrinidad.dev@gmail.com>
2020-07-23 21:30:54 +0800

    ✨ Landing and Nav

--- src/App.svelte
+++ src/App.svelte
@@ -4,16 +4,16 @@
 
 <svelte:head>
 	<title>Titos of Llano</title>
-	<link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Roboto+Slab:wght@300;400;700&display=swap" rel="stylesheet">
 </svelte:head>
 
 <main>
-	<nav class="main-navigation">
-		<a class="logo">
+	<nav class="navigation">
+		<a href="#" class="logo">
 			<img src="./images/logo.svg" alt="">
 			<div class="logo__text">Titos of Llano</div>
 		</a>
-		<ul>
+		<ul class="links">
 			<li><a href="#">Our mission</a></li>
 			<li><a href="#">Our vision</a></li>
 			<li><a href="#">Who we are</a></li>
@@ -21,31 +21,60 @@
 		</ul>
 	</nav>
 	<section class="landing">
-	
+    <div class="landing__logo">
+      <h1>
+        <span>
+          <h2>We are</h2>
+          Titos of Llano
+          <h3>A non-profit organization™</h3>
+        </span>
+      </h1>
+      <img src="./images/mustache.svg" alt="">
+    </div>
+    <div class="landing__scroll">
+      Under maintenance. Come back again soon!
+    </div>
 	</section>
 </main>
 
 <style type="text/scss">
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-		p {
-			color: pink;
-		}
-	}
+  @import './scss/breakpoint';
+  
+  // variables
+  $yellow: #F8EFE0;
+  $yellowish: #FFFAF1;
+  $white: #fff;
+  $whitish-white: #F5F5F5;
+  $just-black: #3B444B;
+  $not-so-black: #414A4C;
+  $grey: #707070;
+  $lesser-grey: #B1B4B7;
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  @mixin font-display($size: 12px, $weight: normal) {
+    font-family: 'Kaushan Script', cursive;
+    font-size: #{$size}px;
+    font-weight: $weight;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @mixin font-text($size: 12px, $weight: normal) {
+    font-family: 'Roboto Slab', serif;
+    font-size: #{$size}px;
+    font-weight: $weight;
+  }
+
+  // global things
+	:global(body), * {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+  }
+  
+  a {
+    text-decoration: none;
+    color: unset;
+  }
+
+
+
+  @import './scss/main';
 </style>
